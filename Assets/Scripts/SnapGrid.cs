3621ec71faea477ad2147ba2dc2841648702b48d
Berk Cambaz <berkcambaz12321@gmail.com>
2021-06-29 19:46:38 +0300

    Fix snap grid

--- Assets/Scripts/SnapGrid.cs
+++ Assets/Scripts/SnapGrid.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class SnapGrid : MonoBehaviour
 {
     public bool snapToCenter = false;
+    public bool snapToCenterX = false;
+    public bool snapToCenterY = false;
 
     void Update()
     {
-        Vector3 pos = new Vector3(
-            RoundTo(transform.position.x, 0.325f, snapToCenter ? 0f : 0.2125f),
-            RoundTo(transform.position.y, 0.325f, snapToCenter ? 0f : -0.2f),
-            RoundTo(transform.position.z, 1f, 0f)
-        );
+        float snappedX;
+        if (snapToCenter || snapToCenterX)
+            snappedX = transform.position.x;
+        else
+            snappedX = transform.position.x - Mathf.Sign(transform.position.x) * 0.2125f;
 
-        transform.position = pos;
+        float snappedY;
+        if (snapToCenter || snapToCenterY)
+            snappedY = transform.position.y;
+        else
+            snappedY = transform.position.y - Mathf.Sign(transform.position.y) * -0.2f;
+
+        snappedX = RoundTo(snappedX, 0.325f, snapToCenter || snapToCenterX ? 0f : 0.2125f);
+        snappedY = RoundTo(snappedY, 0.325f, snapToCenter || snapToCenterY ? 0f : -0.2f);
+
+        transform.position = new Vector3(snappedX, snappedY, transform.position.z);
     }
 
     float RoundTo(float value, float multiplier, float offset)
